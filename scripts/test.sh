efe8886e5f1edc7ae3a735975f66fee8a01e00ca
Oleg Abrosimov <olegabrosimovnsk@gmail.com>
2018-06-14 19:57:47 +0700

    proper wait for ganache to start
    tests fails from time to time otherwise

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
+ganache_port=8545
 ganache-cli -g 0 -l 0 > /dev/null & 
 ganachecli_pid=$!
-echo "Start ganache-cli pid: $ganachecli_pid and sleep 3 seconds"
+echo "Start ganache-cli pid: $ganachecli_pid and wait for it to start"
 
-sleep 3
+echo "Waiting ganache to launch on ${ganache_port}..."
+
+# see https://stackoverflow.com/a/48646792/4256005
+while ! timeout 1 bash -c "echo > /dev/tcp/localhost/${ganache_port}"; do   
+  sleep 1
+done
+
+echo "ganache launched"
 
 vendor/bin/phpunit --coverage-clover=coverage.xml
 ret=$?
 
 kill -9 $ganachecli_pid
-echo "Kill ganache-cli"
+echo "Kill ganache-cli: ${ganachecli_pid}"
 
 exit $ret
